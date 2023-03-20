188c60db02f87fd411ae0e01c366d9b3e63ef9a0
戈亓 <1994975650@qq.com>
2023-03-20 13:01:04 +0800

    photo detection finished

--- user/views.py
+++ user/views.py
@@ -5,6 +5,9 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from user.models import User
 from user.util import Response
+from PIL import Image
+import base64
+from io import BytesIO
 
 
 def Login(request):
@@ -27,52 +30,42 @@ def Regist(request):
         print(name, password)
         # result = User.objects.create(name=name, password=password)
 
-from yolov5.detect_qt5 import my_lodelmodel,main_detect
+
+from yolov5.detect_qt5 import my_lodelmodel, main_detect
 import cv2
 import base64
-def imagePredict(request):
-    if request.method == "GET":
-        fileData = request.GET.get("image")
-        print(33333, fileData)
-        # with open("yolov5/data/images/index.jpg", 'wb+') as f:
-        #     # 分块写入文件
-        #     for chunk in fileData.chunks():
-        #         f.write(chunk)
-        # bytes = read_into_buffer("yolov5/data/images/index.jpg")
-        # print(1111, type(bytes))
-        # im0,label = main_detect(my_lodelmodel(), "yolov5/data/images/index.jpg")
-        im0, label = main_detect(my_lodelmodel(), fileData)
-        print(im0, label)
-
-        width = im0.shape[1]
-        height = im0.shape[0]
-
-        # 设置新的图片分辨率框架
-        width_new = 700
-        height_new = 500
-
-        # 判断图片的长宽比率
-        if width / height >= width_new / height_new:
-
-            show = cv2.resize(im0, (width_new, int(height * width_new / width)))
-        else:
-
-            show = cv2.resize(im0, (int(width * height_new / height), height_new))
-        im0 = cv2.cvtColor(show, cv2.COLOR_RGB2BGR)
-        from PIL import Image
-        im = Image.fromarray(im0)
-        im.save("index.jpg")
-        with open("index.jpg", 'rb') as f:
-            img_data = f.read()
-        base64_data = base64.b64encode(img_data)
-        base64_str = base64_data.decode('utf-8')
-        return HttpResponse(Response(code=200, data=base64_str, message=label))
-
-    return None
 import os
-def read_into_buffer(filename):
-    buf = bytearray(os.path.getsize(filename))
-    with open(filename, 'rb') as f:
-        f.readinto(buf)
-    f.close()
-    return buf
+
+
+def imagePredict(request):
+    if request.method == "POST":
+        fileData = request.FILES['image']
+        filePath = f"yolov5/data/{fileData}"
+        with open(filePath, 'wb+') as f:
+            for chunk in fileData.chunks():
+                f.write(chunk)
+        try:
+            im0, label = main_detect(my_lodelmodel(), filePath)
+            width = im0.shape[1]
+            height = im0.shape[0]
+            # 设置新的图片分辨率框架
+            width_new = 700
+            height_new = 500
+            # 判断图片的长宽比率
+            if width / height >= width_new / height_new:
+                show = cv2.resize(im0, (width_new, int(height * width_new / width)))
+            else:
+                show = cv2.resize(im0, (int(width * height_new / height), height_new))
+            im0 = cv2.cvtColor(show, cv2.COLOR_RGB2BGR)
+            image = Image.fromarray(im0)
+            img_buffer = BytesIO()
+            image.save(img_buffer, format='JPEG')
+            byte_data = img_buffer.getvalue()
+            base64_data = base64.b64encode(byte_data)
+            base64_str = base64_data.decode('utf-8')
+            return HttpResponse(Response(code=200, data=base64_str, message=label))
+        except:
+            return HttpResponse(Response(code=200, data=None, message="图片检测错误"))
+        finally:
+            os.remove(filePath)
+    return None
